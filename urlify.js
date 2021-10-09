b39f87f1a11194ac233fa13b9fb4825074981601
chimailo <collins.chima.ilo@gmail.com>
2021-10-09 13:52:24 +0100

    Task 3: urlify.js

---
+++ urlify.js
@@ -0,0 +1,17 @@
+// Method to replace all spaces in a string with %20
+// params:
+//   str: String - string to carry out operation on
+//   target: String - what to replace
+//   replaceWith: String - what to replace target with
+function urlify(str, target=' ', replaceWith='20%') {
+    for (i=0; i<str.length; i++) {
+        str = str.replace(target, replaceWith);
+    }
+    return str;
+    // return str.replace(/ /g, replaceWith)
+}
+
+
+console.log(urlify('Mr John Smith'))
+console.log(urlify('A rather pleasant opportunity'))
+console.log(urlify(`That's just the way it is`))
